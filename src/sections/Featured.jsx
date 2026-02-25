ff761ed1ca4140bb111241058eb1a61536987f68
Rittik Srivastav <your-github-rittiksrivastav9140@gmail.com>
2026-02-25 17:42:05 +0530

    Fixed featured image path

--- src/sections/Featured.jsx
+++ src/sections/Featured.jsx
@@ -3,11 +3,11 @@ export default function Featured() {
     <div className="container-fluid my-5">
       <div className="row">
         <div className="col-md-6 text-center">
-          <img src="/images/men.jpg" className="img-fluid" />
+          <img src="images/men.jpg" className="img-fluid" />
           <h3>Shop Men's Collection</h3>
         </div>
         <div className="col-md-6 text-center">
-          <img src="/images/women.jpg" className="img-fluid" />
+          <img src="images/women.jpg" className="img-fluid" />
           <h3>Shop Women's Collection</h3>
         </div>
       </div>
